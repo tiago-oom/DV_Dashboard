913d36078d02d326aedda23b39cd8a561b807775
tiago-oom <90836662+tiago-oom@users.noreply.github.com>
2022-04-17 22:57:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
 
